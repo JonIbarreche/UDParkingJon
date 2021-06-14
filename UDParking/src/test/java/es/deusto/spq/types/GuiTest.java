e7f3577677d7a9a53ce7d5f1bb5cdfcffe354c27
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-06-14 03:43:25 +0200

    GuiTest & IntegrationTest añadidos + cambios en el pom

---
+++ UDParking/src/test/java/es/deusto/spq/types/GuiTest.java
@@ -0,0 +1,12 @@
+/** \file 
+ * Breve descripción de es.deusto.spq.types GuiTest.java. Jun 13, 2021
+ */
+package es.deusto.spq.types;
+
+/**
+ * Interface test GuiTest
+ *
+ */
+public interface GuiTest {
+
+}
---
+++ UDParking/src/test/java/es/deusto/spq/types/IntegrationTest.java
@@ -0,0 +1,13 @@
+/** \file 
+ * Breve descripción de es.deusto.spq.types IntegrationTest.java. Jun 13, 2021
+ */
+package es.deusto.spq.types;
+
+/**
+ * Interface test IntegrationTest
+ *
+ */
+public interface IntegrationTest {
+
+}
+
