7e79683123efa0d095ec45dbd06533b0a37e3c01
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-06-14 18:14:10 +0200

    database.sql cambios

--- UDParking/src/main/resources/database.sql
+++ UDParking/src/main/resources/database.sql
@@ -1,4 +1,4 @@
 DROP SCHEMA parking;
 CREATE DATABASE parking;
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
-GRANT ALL ON cine.* TO 'spq'@'localhost';
+GRANT ALL ON parking.* TO 'spq'@'localhost';
